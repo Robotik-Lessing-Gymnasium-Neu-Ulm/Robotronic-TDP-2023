5c56882a199f44aed0e64d9e6874ae2e621f9778
Elias Braun <braunelias@tghd.email>
2023-05-12 18:56:49 +0200

    fixed Issue

--- upload/deploy.sh
+++ upload/deploy.sh
@@ -1,4 +1,4 @@
 zip deploy ../Robotronic2023.pdf index.html main.css 247.jpg 
 
-curl -X POST -F "file=@deploy.zip" https://docat.tghd.eu/api/Robotronic-2023/1.0.0 -H 
+curl -X POST -F "file=@deploy.zip" https://docat.tghd.eu/api/Robotronic-2023/1.0.0 
     
