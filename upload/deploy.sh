1afed7d3722141980f3f43a932bd8fe0ddccdda0
Elias Braun <braunelias@tghd.email>
2023-05-13 23:26:30 +0200

    Added Secrurity Key

--- upload/deploy.sh
+++ upload/deploy.sh
@@ -1,4 +1,4 @@
 zip deploy ../Robotronic2023.pdf index.html main.css 247.jpg 
 
-curl -X POST -F "file=@deploy.zip" https://docat.tghd.eu/api/TeamRobotronic-2023/1.0.0 
+curl -X POST -F "file=@deploy.zip" https://docat.tghd.eu/api/TeamRobotronic-2023/1.0.0 -H "docat-api-key: 084ca8bd6c050c28b10f3ebb2d49172f"
     
